f79109feca217103f71b7975a0913562b8033d00
kita <kita@kita-PC>
2016-12-01 17:32:47 +0900

    good

---
+++ MovieProject/src/sources/Here.java
@@ -0,0 +1,15 @@
+package sources;
+
+public class Here {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Here();
+
+	}
+
+	public Here() {
+		System.out.println("Hello Git!");
+	}
+
+}
